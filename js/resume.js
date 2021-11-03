500b0096b133636071dc7dbcf54ce57b017a8982
phury <phvdvelde@gmail.com>
2021-11-03 20:34:20 +0100

    Move to hugo

--- js/resume.js
+++
@@ -1,7 +0,0 @@
-$.getJSON( "assets/resume.json", function( resume ) {
-	let tpl = document.getElementById('handlebars');
-	// console.log(resume);
-	let template = Handlebars.compile(tpl.innerHTML);
-	let elt = document.getElementById('handlebars');
-	tpl.insertAdjacentHTML('beforebegin', template({ resume: resume }));
-});
