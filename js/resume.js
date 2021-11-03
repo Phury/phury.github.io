e73a682f380f46f91fca151e5a1819bbdc7b3b00
phury <phvdvelde@gmail.com>
2021-11-03 21:32:56 +0100

    Update color scheme and resume

--- js/resume.js
+++ js/resume.js
@@ -1,3 +1,13 @@
+Handlebars.registerHelper('ifeq', function (a, b, options) {
+    if (a == b) { return options.fn(this); }
+    return options.inverse(this);
+});
+Handlebars.registerHelper('ifnoteq', function (a, b, options) {
+	console.log('ifnoteq: ' + a + ' ' +  b);
+    if (a != b) { return options.fn(this); }
+    return options.inverse(this);
+});
+
 $.getJSON( "assets/resume.json", function( resume ) {
 	let tpl = document.getElementById('handlebars');
 	// console.log(resume);
